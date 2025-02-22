63e8cf7986ca6d2c027e0e71452d46eab568d411
PRAVALLIKAMARRI <pravallikamarri1@gmail.com>
2025-02-22 23:50:30 +0530

    src

---
+++ src/components/Landing.js
@@ -0,0 +1,20 @@
+
+function Landing() 
+{
+return(<>
+<div className='content'>
+          <h2>Welcome to Auction App</h2>
+          <p>
+          An Auction is the process of selling or buying goods or services by inviting competing 
+
+          bids from multiple service providers or vendors. 
+          The competing participants may or may not know the identities or actions of other competitors.
+          </p>
+
+          
+          </div>
+          </>
+)
+}
+
+export default Landing
